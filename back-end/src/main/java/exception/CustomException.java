4f65ea9eb6afd0de9eca5de866705cb58912cd4a
Augusto Silva <augusto_ls17@hotmail.com>
2020-01-02 21:28:25 -0300

    Criação de classes para tratamento de exceção e das classes services de usuário

---
+++ back-end/src/main/java/exception/CustomException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class CustomException extends RuntimeException {
+
+    public CustomException(String exception){
+        super(exception);
+    }
+}
---
+++ back-end/src/main/java/service/imp/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package service.imp;
+
+import exception.CustomException;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import repository.UserRepository;
+import service.UserService;
+import util.Constants;
+import util.Response;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User insertUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User alterUser(User user, Long id) {
+        if(Objects.isNull(id)){
+            throw new CustomException(Constants.ID_REQUIRED);
+        }
+
+        Optional<User> u = userRepository.findById(id);
+
+        if(u.isPresent()){
+            return userRepository.save(user);
+        }else{
+            throw new CustomException(Constants.USER_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Response deleteById(Long id) {
+        if(Objects.isNull(id)){
+            throw new CustomException(Constants.ID_REQUIRED);
+        }
+
+        Optional<User> u = userRepository.findById(id);
+
+        if(u.isPresent()){
+            userRepository.deleteById(id);
+            return new Response("suceso", "Usuário excluído com sucesso.");
+        }else{
+            throw new CustomException(Constants.USER_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public User findById(Long id) {
+        if(Objects.isNull(id)){
+            throw new CustomException(Constants.ID_REQUIRED);
+        }
+
+        return userRepository.findById(id).get();
+    }
+}
