08c6f20ba524bbb00bc45c111200070723eb4eaf
Augusto Silva <augusto_ls17@hotmail.com>
2020-01-02 21:10:57 -0300

    Criação de classes úteis, que serão necessárias no desenvolvimento futuro

---
+++ back-end/src/main/java/util/Constants.java
@@ -0,0 +1,7 @@
+package util;
+
+public final class Constants {
+    public static final String USER_NOT_FOUND = "Usuário não encontrado.";
+    public static final String DELETED_SUCCESSFUL = "Registro deletado com sucesso.";
+    public static final String ID_REQUIRED = "É necessário informar o ID.";
+}
